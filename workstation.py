130aeebb22eaeb5acc45c0f756ff5e041527a35f
Relizc <3464840574@qq.com>
2024-01-12 11:21:29 +0800

    Studio
    studio

---
+++ workstation.py
@@ -0,0 +1,51 @@
+from tkinter import ttk
+from tkinter import filedialog
+import tkinter as tk
+import threading
+import os
+
+class Project:
+
+    def __init__():
+        pass
+
+def create_empty_project
+
+
+#s = ttk.Style()
+
+root = tk.Tk()
+root.geometry("640x360")
+root.title("PyNamics Workstation - New")
+
+greeting0 = tk.Label(root, font=("Segoe UI", 24), text="PyNamics Workstation (v1.0)")
+greeting0.pack()
+
+greeting1 = tk.Label(root, font=("Segoe UI", 12), text="Gonna frick some games")
+greeting1.pack()
+
+def ask_open_dir(entry):
+    p = filedialog.askdirectory()
+    entry.set(p)
+    
+
+def main():
+    op = tk.Toplevel(root, padx=8,pady=8)
+    op.geometry("480x80")
+
+    nt = tk.StringVar(value=os.getcwd())
+
+    tk.Label(op, text="File Directory").grid(row=0, column=0, sticky="E")
+    L = tk.Entry(op, textvariable=nt, width=32)
+    L.grid(row=0, column=1, sticky="W")
+
+    tk.Button(op, text="Browse", width=12, command=lambda: ask_open_dir(nt)).grid(row=2, column=0, sticky="E")
+    tk.Button(op, text="Create", width=12).grid(row=2, column=1, sticky="W")
+    tk.Button(op, text="Open", width=12, default="active").grid(row=2, column=1, sticky="E")
+
+
+    op.mainloop()
+
+root.after(100, main)
+root.mainloop()
+
