f5e94b613826ae45b815b9004eede4035626c8bc
Relizc <3464840574@qq.com>
2024-01-16 19:07:24 +0800

    d
    d

--- test_smoothblit.py
+++ test_smoothblit.py
@@ -1,25 +1,6 @@
-import pygamepro
-import random
+import pynamics as pn
 
-x = pygamepro.GameObject()
-ctx = pygamepro.GameContext.from_dim(pygamepro.Dimension(600, 800), styles = {
-    "background-color": "white"
-}, tick = 2, maxfps=144)
+a = pn.Vector(180, 3)
+b = pn.Vector(90, 4)
 
-n = 0
-
-test = ctx.create_rect(pygamepro.Dimension(300, 400), pygamepro.Dimension2d(0, 10, 0, 10), smooth_blit = True)
-test2 = ctx.create_rect(pygamepro.Dimension(300, 400), pygamepro.Dimension2d(0, 10, 0, 10), smooth_blit = False, styles = {
-    "background-color": (255, 0, 0)
-})
-
-@test.addEventListener("update")
-def onrender(self):
-    self.x = random.randint(0, 590)
-    self.y = random.randint(0, 590)
-
-    test2.x = self.x
-    test2.y = self.y
-
-ctx.start()
-
+print(a.add(b))
